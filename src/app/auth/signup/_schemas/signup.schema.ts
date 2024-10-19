329049c31523de7e6c9fdde08d4eab494f730f03
Damoz1606 <panchitodmun@gmail.com>
2024-10-19 14:49:22 -0500

    Signup validation

--- src/app/auth/signup/_schemas/signup.schema.ts
+++ src/app/auth/signup/_schemas/signup.schema.ts
@@ -3,9 +3,21 @@ import { z } from "zod";
 export default z.object({
     email: z.string().email({
         message: 'This field must be an email'
+    }).max(64, {
+        message: 'Email is too long. Max 64 char.'
     }),
-    username: z.string(),
+    username: z.string().min(1, {
+        message: 'Username cannot be empty'
+    }).max(64, {
+        message: 'Username is too long. Max 64 char.'
+    }),
     password: z.string().min(8, {
-        message: "Username must be at least 8 characters.",
-    }),
+        message: "Password must be at least 8 characters.",
+    }).refine((value) => /[A-Z]/.test(value), {
+        message: 'Password must contain at least one uppercase'
+    }).refine((value) => /[0-9]/.test(value), {
+        message: 'Password must contain at least one number'
+    }).refine((value) => /[!@#$%^&*(),.?":{}|<>]/.test(value), {
+        message: 'Password must contain at least one special character'
+    })
 })
